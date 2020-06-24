0585878e3c643b0a97809cfc3dde5476d60c9ec2
gyunbox <gyunbox@naver.com>
2020-06-24 21:57:14 +0900

    노트북 커밋

---
+++ Gyunbox40/Models/DaMember.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using Gyunbox40.Common;
+
+namespace Gyunbox40.Models
+{
+    public class DaMember
+    {
+        public DataSet GetMember()
+        {
+            DataSet ds = new DataSet();
+            DBConn db = new DBConn();
+            DaMember daMem = new DaMember();
+            string strQuery = "select * from hope20603.member;";
+            try
+            {
+                ds = db.ExecuteWithDataSet(strQuery);
+                db.Close();
+            }
+            catch(Exception e)
+            {
+                
+            }
+            finally
+            {
+                if(db != null)
+                {
+                    db.Close();
+                }
+            }
+            
+            return ds;
+        }
+    }
+
+}
